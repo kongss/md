1f3242cc3852890fac65c0bdd211c46e5eb06b51
kongshuaishuai <kong10455>
2017-09-22 16:27:37 +0800

    上传 md 项目

---
+++ md/js/md.js
@@ -0,0 +1,16 @@
+var md = function (id) {
+    new md.fn.init(id);
+};
+
+md.prototype = md.fn={
+    /*init : function (id) {
+        console.log("id========="+id);
+        //var tVal = $(".ace_text-input").val();
+
+    },
+    save : function (val) {
+        var v =md.marked(val);
+        console.log("v==========="+v);
+    }*/
+
+}
